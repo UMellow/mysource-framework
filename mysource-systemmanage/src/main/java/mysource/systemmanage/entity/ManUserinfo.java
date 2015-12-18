737b284d7fb313dcf9e2e1f4f3ac0426a1d59d27
UMellow <304159633@qq.com>
2015-12-18 10:04:04 +0800

    ADD

---
+++ mysource-systemmanage/src/main/java/mysource/systemmanage/entity/ManUserinfo.java
@@ -0,0 +1,59 @@
+package mysource.systemmanage.entity;
+
+public class ManUserinfo {
+
+	private Long iid;
+	private String sloginname;
+	private String sloginpass;
+	private String srealname;
+	private String spersoncradid;
+	private String sareaid;
+
+	public Long getIid() {
+		return iid;
+	}
+
+	public void setIid(Long iid) {
+		this.iid = iid;
+	}
+
+	public String getSloginname() {
+		return sloginname;
+	}
+
+	public void setSloginname(String sloginname) {
+		this.sloginname = sloginname;
+	}
+
+	public String getSloginpass() {
+		return sloginpass;
+	}
+
+	public void setSloginpass(String sloginpass) {
+		this.sloginpass = sloginpass;
+	}
+
+	public String getSrealname() {
+		return srealname;
+	}
+
+	public void setSrealname(String srealname) {
+		this.srealname = srealname;
+	}
+
+	public String getSpersoncradid() {
+		return spersoncradid;
+	}
+
+	public void setSpersoncradid(String spersoncradid) {
+		this.spersoncradid = spersoncradid;
+	}
+
+	public String getSareaid() {
+		return sareaid;
+	}
+
+	public void setSareaid(String sareaid) {
+		this.sareaid = sareaid;
+	}
+}
