7f175da0d4f88fdf3d1ec6e61f8a2adf6d512b47
UMellow <304159633@qq.com>
2016-10-12 09:52:22 +0800

    2016.10.12 09:52

--- mysource-busi/src/main/java/mysource/busi/xydtech/lns/service/ScopeSingletonBean.java
+++ mysource-busi/src/main/java/mysource/busi/xydtech/lns/service/ScopeSingletonBean.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 @Service
-@Scope("request")
+//@Scope("request")
 public class ScopeSingletonBean {
 	
 	private Integer counter;
